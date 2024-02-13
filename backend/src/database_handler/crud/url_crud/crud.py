b7d0b395827e5c8096e5627a55a02e58fd8f96cc
Rohit Karan (rohikara) <rohikara@cisco.com>
2024-02-13 22:02:29 +0530

    Update

--- backend/src/database_handler/crud/url_crud/crud.py
+++ backend/src/database_handler/crud/url_crud/crud.py
@@ -6,15 +6,30 @@ from sqlalchemy import func
 from base62conversions.base62conversions import decimal_to_base62 , base62_to_decimal
 from database_handler.db_connector import db_connector
 from sqlalchemy import MetaData
+from sqlalchemy import update
 
 def create_short_url(db: Session , create_url: NEW_URL_REQUEST, email: str):
     try:
-        url_obj = URLS_Mapping(long_url=create_url.long_url, email=email)
-        db.add(url_obj)
-        db.commit()
-        db.refresh(url_obj)
+        existing_url = db.query(URLS_Mapping).filter(URLS_Mapping.email.is_('NULL')).first()
+        short_url = None
+        
+        if existing_url:
+            
+            update_data = {
+                "long_url": create_url.long_url,
+                "email": email
+            }
+            db.execute(update(URLS_Mapping).where(URLS_Mapping.id == existing_url.id).values(update_data))
+            db.commit()
+            short_url = "http://localhost:8000/" + decimal_to_base62(existing_url.id)
+        else:
+            url_obj = URLS_Mapping(long_url=create_url.long_url, email=email)
+            db.add(url_obj)
+            db.commit()
+            db.refresh(url_obj)
     
-        short_url = "http://localhost:8000/" + decimal_to_base62(url_obj.id)
+            short_url = "http://localhost:8000/" + decimal_to_base62(url_obj.id)
+            
         return {"short_url": short_url}
     except Exception as e:
         Exception(f"Error creating short url:{e}")
@@ -26,3 +41,16 @@ def get_original_url(db: Session, short_url: str):
     if item is None:
         raise HTTPException(status_code=404, detail="Item not found")
     return item.long_url
+
+def delete_url(db: Session, long_url: str, email: str):
+    try:
+        email_to_update = 'NULL'
+        long_url_to_update = 'NULL'
+        db.execute(
+            update(URLS_Mapping).
+            where((URLS_Mapping.email == email) & (URLS_Mapping.long_url ==long_url)).  # Filter based on the old email
+            values(email=email_to_update, long_url=long_url_to_update)
+        )
+        db.commit()
+    except Exception as e:
+        raise Exception(f"Error deleting url: {e}")
--- backend/src/routes/url_routes/routes.py
+++ backend/src/routes/url_routes/routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Body, Response, Request
+from fastapi import APIRouter, Depends, Body
 from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 
@@ -9,9 +9,7 @@ from utils.send_response import send_response
 from logger import logger
 from decorators import log_info
 
-from database_handler.crud import create_short_url, get_original_url
-from auth import OAuth2PasswordBearerWithCookie
-from typing import Annotated
+from database_handler.crud import create_short_url, get_original_url, delete_url
 from auth import auth_handler
 
 router = APIRouter(
@@ -19,14 +17,25 @@ router = APIRouter(
     tags=["url"],
 )
 
-oauth2_scheme = OAuth2PasswordBearerWithCookie(tokenUrl="/user/login")
-
 @router.post("/create_url")
-def add_url(new_url: NEW_URL_REQUEST  = Body(default=None) ,  db: Session = Depends(db_connector.get_db), token: str = Depends(oauth2_scheme)):
+def add_url(new_url: NEW_URL_REQUEST  = Body(default=None) ,  db: Session = Depends(db_connector.get_db), token: str = Depends(auth_handler.O2AUTH2_SCHEME)):
     try:
         user = auth_handler.get_current_user(token)
         obj = create_short_url(db ,create_url=new_url, email = user.get('email'))
         return obj
+    except Exception as e:
+        return send_response(content={"error": e}, status_code=500, error_tag=True)
+
+@router.delete("/delete_url")
+def delete_long_url(url_to_delete: NEW_URL_REQUEST = Body(default=None), db: Session = Depends(db_connector.get_db), token: str = Depends(auth_handler.O2AUTH2_SCHEME)):
+    try:
+        user = auth_handler.get_current_user(token)
+        delete_url(db , long_url=url_to_delete.long_url ,email=user.get('email'))
+        
+        content = {
+            "message": "URL deleted successfully"
+        }
+        return send_response(content=content, status_code=200)
     except Exception as e:
         return send_response(content={"error": e}, status_code=500, error_tag=True)
 
