178949b7eb4095600b4bb9aa15fe6d8fbd84592d
kaustubhshukla30 <kaustubhshukla30@gmail.com>
2024-02-16 19:44:20 +0530

    code optimization

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,72 +5,67 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
-import {useSelector , useDispatch} from 'react-redux';
-import { getUser , logoutUser } from '../utils/api';
-import { addUser , removeUser } from  "../redux/userSlice";
+import { useSelector, useDispatch } from 'react-redux';
+import { getUser, logoutUser } from '../utils/api';
+import { addUser, removeUser } from  "../redux/userSlice";
 import "./Header.css";
 
-const  Header = () => {
+const Header = () => {
     const navigate = useNavigate();
     const user = useSelector((store) => store.user);
     const dispatch = useDispatch();
 
     useEffect(() => {
-      const helper = async () => {
-        try{
-          const res = await getUser();
-          if(res.status === 200){
-            if(!user){
-              dispatch(addUser(JSON.parse(res.data)));
-              navigate('/');
+        const fetchUser = async () => {
+            try {
+                const res = await getUser();
+                if (res.status === 200 && !user) {
+                    dispatch(addUser(JSON.parse(res.data)));
+                    navigate('/');
+                }
+            } catch (err) {
+                console.error("Error checking user status:", err);
             }
-          }
-        }catch(err){
-          console.log("User not logged in");
+        };
+
+        fetchUser();
+    }, [user, dispatch, navigate]);
+
+    const handleLogout = async () => {
+        try {
+            await logoutUser();
+            dispatch(removeUser());
+            navigate('/login');
+        } catch (err) {
+            console.error("Error logging out:", err);
         }
-      }
-      helper();
-    });
+    };
 
-    useEffect(()=> {
-      if(!user){
+    const handleSignIn = () => {
         navigate('/login');
-      }
-    }, [user]);
+    };
 
-    const handleClick = (e) =>{
-        e.preventDefault();
-        navigate('/login');
-    }
-    const handleLogout = async() =>{
-      try{
-        const res = await logoutUser();
-        console.log(res);
-        dispatch(removeUser());
-        navigate('/login');
-      }catch(err){
-        console.log(err);
-      }
-    }
-    const handleLogoClick = (e) =>{
-      e.preventDefault();
-      navigate('/');
-    }
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} onClick={(e) => handleLogoClick(e)} className = 'heading'>
-            URL SHORTENER
-          </Typography>
-          {
-            !user ? <Button color="inherit" onClick={handleClick}>Sign in</Button> : <Button color="inherit" onClick={handleLogout}>Logout</Button>
-          }
-          <Button color="inherit" onClick={() => navigate('/books')}>Books</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
-}
+    const handleLogoClick = () => {
+        navigate('/');
+    };
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} onClick={handleLogoClick} className="header-title">
+                        URL SHORTENER
+                    </Typography>
+                    {
+                        !user ?
+                        <Button color="inherit" onClick={handleSignIn}>Sign in</Button> :
+                        <Button color="inherit" onClick={handleLogout}>Logout</Button>
+                    }
+                    <Button color="inherit" onClick={() => navigate('/books')}>Books</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+};
 
 export default Header;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,98 +1,86 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import './Login.css';
-import { useState, useRef } from 'react';
-import checkValidate from "../utils/validate";
-import { loginUser, getUser,  createUser} from '../utils/api';
+import  checkValidate from '../utils/validate';
+import { loginUser, getUser, createUser } from '../utils/api';
 import { useNavigate } from 'react-router-dom';
 import { addUser } from '../redux/userSlice';
 import { useDispatch } from 'react-redux';
 
-
 const Login = () => {
     const [isSignIn, setIsSignIn] = useState(true);
-    const [errorMessage, setErrorMessage] = useState(null)
-    const name = useRef();
-    const email = useRef();
-    const password = useRef();
+    const [errorMessage, setErrorMessage] = useState(null);
+    const nameRef = useRef('');
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
-
     const toggleSignInForm = () => {
         setIsSignIn(!isSignIn);
-    }
+    };
 
-  const handleSubmit = async (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        const err = checkValidate(email.current.value, password.current.value);
+        const err = checkValidate(emailRef.current.value, passwordRef.current.value);
         setErrorMessage(err);
-        if (err) {
-            return;
-        }
-        const formData = new FormData();
-        formData.append('username' , email.current.value);
-        formData.append('password' , password.current.value);
-        let rc = 200;
-        if(!isSignIn){
-            try{
-            const res = await createUser({"name": name.current.value , "email" : email.current.value , "password": password.current.value});
-            rc = res.status;
-            }
-            catch(err){
-            rc = err?.response?.status;
-            console.log(err);
-            }
-        }
+        if (err) return;
 
-        if(rc !== 200){
-            return;
-        }
-        try{
-            const res = await loginUser(formData);
-            if(res.status === 200){
-                rc = 200;
-                const user = await getUser();
-                dispatch(addUser(JSON.parse(user.data)));
+        try {
+            let rc = 200;
+            if (!isSignIn) {
+                const res = await createUser({ "name": nameRef.current.value, "email": emailRef.current.value, "password": passwordRef.current.value });
+                rc = res.status;
             }
-        }
-        catch (err){
-            rc = err?.response?.status;
-            console.log(err);
-        }
-        if(rc !== 200){
-            return;
-        }
-        email.current.value = null;
-        password.current.value = null;
-        navigate('/');
-    }
 
-  return (
-      <Box
-        component = "form"
-        sx={{
-          '& .MuiTextField-root': {m:2, width: '50ch' },
-          display: 'flex',
-          justifyContent: 'center',
-          flexDirection: 'column'
-        }}
-        autoComplete="off"
-        className='box'
-        onSubmit = {(e) => handleSubmit(e)}
-      >
-        {!isSignIn && (<TextField id="outlined-basic" label="Name" variant="outlined" inputRef = {name} type = "input"  required />)}
-            <TextField id="outlined-basic" label="Email" variant="outlined"  inputRef = {email} type = "input"  required />
-            <TextField id="filled-basic" label="Password" type = "password" variant="outlined" inputRef = {password}  required/>
-            <Button variant="contained" size="medium" style={ {width: '65ch'}} type = "submit" >
-                { isSignIn ? `Login`: 'Signup'}
+            if (rc === 200) {
+                const formData = new FormData();
+                formData.append('username', emailRef.current.value);
+                formData.append('password', passwordRef.current.value);
+                const res = await loginUser(formData);
+
+                if (res.status === 200) {
+                    const user = await getUser();
+                    dispatch(addUser(JSON.parse(user.data)));
+                    emailRef.current.value = '';
+                    passwordRef.current.value = '';
+                    navigate('/');
+                } else {
+                    console.log('Login failed');
+                }
+            } else {
+                console.log('User creation failed');
+            }
+        } catch (err) {
+            console.error('Error:', err);
+        }
+    };
+
+    return (
+        <Box
+            component="form"
+            sx={{
+                '& .MuiTextField-root': { m: 2, width: '50ch' },
+                display: 'flex',
+                justifyContent: 'center',
+                flexDirection: 'column'
+            }}
+            autoComplete="off"
+            className='box'
+            onSubmit={(e) => handleSubmit(e)}
+        >
+            {!isSignIn && (<TextField id="outlined-basic" label="Name" variant="outlined" inputRef={nameRef} type="input" required />)}
+            <TextField id="outlined-basic" label="Email" variant="outlined" inputRef={emailRef} type="input" required />
+            <TextField id="filled-basic" label="Password" type="password" variant="outlined" inputRef={passwordRef} required />
+            <Button variant="contained" size="medium" style={{ width: '65ch' }} type="submit">
+                {isSignIn ? `Login` : 'Signup'}
             </Button>
-            <p className = 'para' onClick={toggleSignInForm}>{ isSignIn ? `New User? Signup`: 'Already a user? Signin'}</p>
-            <p className = 'para' >{errorMessage}</p>
-      </Box>
-  )
-}
+            <p className='para' onClick={toggleSignInForm}>{isSignIn ? `New User? Signup` : 'Already a user? Signin'}</p>
+            <p className='para'>{errorMessage}</p>
+        </Box>
+    );
+};
 
-export default Login
+export default Login;
